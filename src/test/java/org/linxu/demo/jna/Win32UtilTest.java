6f7b9b6e74f175f70452c3701712990f6a113234
linxu <linxu@qq.com>
2022-07-05 08:48:14 +0800

    update

--- src/test/java/org/linxu/demo/jna/Win32UtilTest.java
+++ src/test/java/org/linxu/demo/jna/Win32UtilTest.java
@@ -4,12 +4,20 @@ import com.sun.jna.platform.win32.WinDef;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 public class Win32UtilTest {
     @Test
     public void test01() {
         WinDef.HWND hwnd = Win32Util.findHandleByClassName("RichEditD2DPT");
         Assertions.assertNotNull(hwnd);
-        boolean ok = Win32Util.simulateTextInput(hwnd, "test6");
+        boolean ok = Win32Util.simulateTextInput(hwnd, "test10");
         Assertions.assertTrue(ok);
+        StringSelection stringSelection = new StringSelection("abc");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+        int[][] keyCodes = {{KeyEvent.VK_CONTROL, KeyEvent.VK_V}};
+        Win32Util.simulateKeyboardEvent(hwnd, keyCodes);
     }
 }
